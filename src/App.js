e076cc4ab21ee7191350b792a10376eca85ceceb
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-16 16:51:38 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@
 import './App.css';
 
 function App() {
+  let title = 'Welcome to the blog'
+  let likes = 50;
+  let person = {
+    name:'mera',
+    age:20,
+  }
+  let link ='http://www.google.com';
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>{ title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> //it gives error we cant give string to them  */}
+
+        <p>{10}</p>
+        <p>{'hello buddes'}</p>
+        <p>{[1,2,3,4,5,6]}</p>
+        <a href={link}>Click here</a>
       </div>
     </div>
   );
