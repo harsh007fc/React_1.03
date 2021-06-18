5218532149d3e2ba667b82c6ddc46387fa597754
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-18 11:38:10 +0530

    added upto click events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 
 const Home = () => {
+
+    let handleClick = (e) => {
+        console.log('Hello , Friends',e);
+    }
+    let handleSecondClick = (name , e) => {
+        console.log('Hello , '+name);
+        console.log(e.target);
+    }
     return (
         <>
            <div className="home">
               <h2>HomePage </h2> 
+              <button onClick={handleClick}>Click Me</button>
+              <button onClick={(e) => {handleSecondClick('Mario',e)}}>Click Me Again</button>
             </div> 
         </>
     )
