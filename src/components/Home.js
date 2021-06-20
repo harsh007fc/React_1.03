7b99de3dd022680ac78b2d18a1eaed0e204c0b5d
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-20 17:08:24 +0530

    upto json server

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,19 +16,13 @@ const Home = () => {
         setBlogs(newBlogs);
     }
     //this is a hook
-    useEffect(()=>{ //this function reuns with every render
-        //prescribed to not change state in this beacause it will call rerenderwhich again call this function and we stuck in a continuous loop
+    useEffect(()=>{ 
         console.log('use effect ran');
-        // console.log(blogs);
-        console.log(name);
-    },[name]);
+    },[]);
     return (
         <>
            <div className="home">
               <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
-              <button onClick={()=>setname('Luigi')}>Change name</button>
-              <p>{name}</p>
-              {/* <BlogList blogs={blogs.filter((blog) => blog.author ==='mario')} title='Mario Blogs'/> */}
             </div> 
         </>
     )
