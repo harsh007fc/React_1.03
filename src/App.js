0601d4c5fc4d6ba288a6447e8cdae132823b61e3
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-23 00:04:52 +0530

    addded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+// npx json-server --watch data/db.json --port 8000
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
 
     let [blogs,setBlogs] = useState(null);
     let [isPending,setIsPending] = useState(true);
+    let [error,setError] = useState(null);
 
 
 
@@ -15,15 +16,24 @@ const Home = () => {
     useEffect(()=>{ 
         fetch('http://localhost:8000/blogs')
         .then(res=>{
+            console.log(res);
+            if(!res.ok){
+                throw Error('Could not fetch the data for that resource')
+            }
            return res.json();
         }).then((data)=>{
             setBlogs(data);
             setIsPending(false); 
-        })
+            setError(null)
+        }).catch((err)=>{
+            setIsPending(false); 
+            setError(err.message);
+        }); 
     },[]);
     return (
         <>
            <div className="home">
+               {error && <div>{ error }</div> }
                {isPending && <div>Loading...</div>}
               { blogs && <BlogList blogs={blogs} title='All Blogs'/>}
             </div> 
